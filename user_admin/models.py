af02e8f30efe19a4601ee22a8bb59161b5ccaf9b
shtewe <shteweahmad95@gmail.com>
2020-04-03 14:49:25 +0300

    b

--- user_admin/models.py
+++ user_admin/models.py
@@ -4,14 +4,27 @@ class Category(models.Model):
     catName = models.CharField(primary_key=True,max_length = 200,unique=True)
     isAvtive = models.BooleanField(default=False)
 
+
     def __str__(self):
         return self.catName + ' | ' + str(self.isAvtive)
 
 
 class User(models.Model):
-    email = models.EmailField(primary_key=True, max_length = 200, unique=True)
+    user_name = models.CharField(max_length = 200)
+    email = models.CharField(max_length = 200, null=True,unique=True)
     password = models.CharField(max_length = 200)
 
 
     def __str__(self):
-        return  self.email + '|' +self.password
+        return self.user_name + ' | ' + self.email + '|' +self.password
+
+class Product(models.Model): 
+    #ProductID = models.CharField(primary_key=True,max_length = 200,unique=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    ProductName= models.CharField(max_length=200)
+    Wight =  models.FloatField()
+    price = models.FloatField()
+    Quantiy = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title + ' | ' +str(self.category) + ' | ' +str(self.price) + ' | ' +str(self.qty) + self.description
+
