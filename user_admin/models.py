4a771a8d2facf092baea9ce4102c93b12bd8be50
AhmadAmara <ahmad23a01@gmail.com>
2020-04-03 14:08:39 +0300

    login and sined up updated

--- user_admin/models.py
+++ user_admin/models.py
@@ -4,16 +4,14 @@ class Category(models.Model):
     catName = models.CharField(primary_key=True,max_length = 200,unique=True)
     isAvtive = models.BooleanField(default=False)
 
-
     def __str__(self):
         return self.catName + ' | ' + str(self.isAvtive)
 
 
 class User(models.Model):
-    user_name = models.CharField(max_length = 200)
-    email = models.CharField(max_length = 200, null=True,unique=True)
+    email = models.EmailField(primary_key=True, max_length = 200, unique=True)
     password = models.CharField(max_length = 200)
 
 
     def __str__(self):
-        return self.user_name + ' | ' + self.email + '|' +self.password
+        return  self.email + '|' +self.password
