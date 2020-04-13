6b463ddc10b174685433ded5d04ae2ac15d33230
AhmadMahameed <17ahmadmah@gmail.com>
2020-04-13 15:16:08 +0300

    showing final price and discount in cart

--- user_admin/views.py
+++ user_admin/views.py
@@ -175,12 +175,22 @@ def cart(request):
             cartid=Cart()
             cartid.User_em=User.objects.get(pk=email)
             cartid.save()
-
+        
         ord_lin = Order_Line.objects.filter(CartId=cartid)
+
+        print('------------------------------------------w----------------------------------------')
+        pro_dis = [(o.ProductID.productanddiscountmembership_set.all()) for o in ord_lin]
+        max_discount=[max([p.product_discount.discount_percent for p in pd]+[0]) for pd in pro_dis]
+        price_after=[(o.price*(100-m)/100) for o,m in zip(ord_lin,max_discount)]
+        all_items=list([(ol,md ,pa) for ol,md,pa in zip(ord_lin,max_discount,price_after)])
         totalprice=0
-        for ol in ord_lin :
-            totalprice += ol.Quantity * ol.price
-        return render(request, 'cart.html', {'all_items': ord_lin,'total_price':totalprice})
+        for p,ol in zip(price_after,ord_lin):
+            totalprice += ol.Quantity * p
+        print(max_discount)
+        print(price_after)
+        print (all_items)
+
+        return render(request, 'cart.html', {'all_items': all_items,'total_price':totalprice})
     else:
         return render(request, 'home.html')
 
